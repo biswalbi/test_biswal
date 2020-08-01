eb6834425f96e161c7197e450cdc5625295ea7a7
Admin <biswal81@gmail.com>
2020-08-01 22:45:57 +0530

    name

--- healthcheck.py
+++ healthcheck.py
@@ -1,3 +1,4 @@
 print ("Hello BIswajit")
 print ("This is my first python program")
-print ("check this build is successfull or not 123456")
+print ("check this build is successfull or not")
+print ("what is your name ?")
